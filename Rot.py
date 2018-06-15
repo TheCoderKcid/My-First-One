15cee063b1821db232a42fdb1e4632bae1fd9012
Ari Poisik <aripoisik@gmail.com>
2018-06-15 16:03:53 -0400

    My first commit the Hub!

---
+++ Rot.py
@@ -0,0 +1,15 @@
+class Orange:
+        def _init_(self, c, s):
+                self.weight=w
+                self.shape=int(s)
+                self.mold=0
+                print("The fruit is good!")
+        def rot(self,days,temp):
+                self.mold=days*temp
+                if self.mold>50:
+                        self.shape=Squishy_Mass
+                        self.weight=Less_Than_Usual
+
+
+        #Type orange=____________
+        #Then type print(orange.mold), print(orange.shape),print(orange.weight)
